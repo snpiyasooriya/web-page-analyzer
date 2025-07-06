0c3d2e945416237247ecebb0b45dbb5e68b36d16
Sayuru Piyasooriya <sayuru@seekahost.co.uk>
2025-07-07 02:09:35 +0530

    chore(main): serverMux implemented.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
-package cmd
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/snpiyasooriya/web-page-analyzer/internal/handler"
+)
+
+func main() {
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", handler.HomePageHandler)
+	router.HandleFunc("POST /analyze", handler.AnalysisHandler)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
