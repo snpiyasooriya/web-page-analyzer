bb3bd02f05de97c324fc2a21e57ca9ca0921b6de
Sayuru Piyasooriya <sayuru@seekahost.co.uk>
2025-07-07 02:02:18 +0530

    chore(analyzer): implemented the chore analyzer functionalities.

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -1,1 +1,83 @@
 package analyzer
+
+import (
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type AnalysisResult struct {
+	HTMLVersion                    string
+	Title                          string
+	Headings                       map[string]int
+	InternalLinksCount             int
+	ExternalLinksCount             int
+	InaccessibleInternalLinksCount int
+	InaccessibleExternalLinksCount int
+	HasLoginForm                   bool
+	Links                          []string
+}
+
+func Analyze(body io.Reader) (*AnalysisResult, error) {
+	doc, err := html.Parse(body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &AnalysisResult{
+		Headings: make(map[string]int),
+	}
+
+	traverseTags(doc, result)
+
+	return result, nil
+}
+
+func traverseTags(n *html.Node, result *AnalysisResult) {
+	if n.Type == html.DoctypeNode {
+		result.HTMLVersion = "HTML5"
+	}
+
+	if n.Type == html.ElementNode {
+		switch n.Data {
+		case "title":
+			if n.FirstChild != nil {
+				result.Title = n.FirstChild.Data
+			}
+		case "h1", "h2", "h3", "h4", "h5", "h6":
+			result.Headings[n.Data]++
+		case "a":
+			for _, attr := range n.Attr {
+				if attr.Key == "href" && attr.Val != "" {
+					result.Links = append(result.Links, attr.Val)
+				}
+			}
+		case "form":
+			if !result.HasLoginForm { // Stop checking once one is found
+				result.HasLoginForm = containsPasswordInput(n)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverseTags(c, result)
+	}
+}
+
+// containsPasswordInput is a helper to recursively check for a password field within a form.
+func containsPasswordInput(n *html.Node) bool {
+	if n.Type == html.ElementNode && n.Data == "input" {
+		for _, attr := range n.Attr {
+			if attr.Key == "type" && strings.ToLower(attr.Val) == "password" {
+				return true
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if containsPasswordInput(c) {
+			return true
+		}
+	}
+	return false
+}
