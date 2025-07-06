47aa1a09186519724e95072d6eebb10a710507b4
Sayuru Piyasooriya <sayuru@seekahost.co.uk>
2025-07-07 02:05:48 +0530

    feat(templates): html templates for index and results.

--- internal/handler/analysis_handler.go
+++ internal/handler/analysis_handler.go
@@ -1,1 +1,34 @@
 package handler
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/snpiyasooriya/web-page-analyzer/internal/service"
+)
+
+var templates = template.Must(template.ParseGlob("template/*.html"))
+
+func HomePageHandler(w http.ResponseWriter, _ *http.Request) {
+	err := templates.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func AnalysisHandler(w http.ResponseWriter, r *http.Request) {
+	analysisService := service.NewAnalysisService()
+	url := r.FormValue(`url`)
+	page, err := analysisService.AnalyzePage(r.Context(), url)
+	fmt.Println(page)
+	if err != nil {
+		return
+	}
+	err = templates.ExecuteTemplate(w, "results.html", page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
